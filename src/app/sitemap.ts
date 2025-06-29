a8ab21a87af1c821f89e52d78a83f30efb6c68a6
De Zaldua Samuel <samuel.dezaldua@gmail.com>
2025-06-29 11:18:30 +0200

    Revert "filter undefined items on sitemap"
    This reverts commit a1740553db16eb997d29c87cb08c0bc6b45491b7.

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -10,55 +10,51 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const events = await getCachedEvents({ limit: 1000, startDate: new Date().toISOString() })
   const cities = await getCities()
   const specialEvents = await getSpecialEvents()
+  const regionsUrls = REGIONS.map((region) => {
+    return {
+      url: `https://goazen.info/concerts/${region}`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly' as const,
+      priority: 0.8,
+    }
+  })
+  const citiesUrls = cities.docs.map((city) => {
+    return {
+      url: `https://goazen.info/concerts/${city.region}/${city.slug}`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly' as const,
+      priority: 1,
+    }
+  })
+  const eventsUrls = events.docs.map((event) => {
+    const eventUrl = buildEventUrl(event)
+    return {
+      url: `https://goazen.info${eventUrl}`,
+      lastModified: new Date(),
+      changeFrequency: 'daily' as const,
+      priority: 1,
+    }
+  })
 
-  const regionsUrls = REGIONS.map((region) => ({
-    url: `https://goazen.info/concerts/${region}`,
-    lastModified: new Date(),
-    changeFrequency: 'monthly' as const,
-    priority: 0.8,
-  }))
-
-  const citiesUrls = cities.docs.map((city) => ({
-    url: `https://goazen.info/concerts/${city.region}/${city.slug}`,
-    lastModified: new Date(),
-    changeFrequency: 'monthly' as const,
-    priority: 1,
-  }))
-
-  const eventsUrls = events.docs
-    .map((event) => {
-      const eventUrl = buildEventUrl(event)
-      if (!eventUrl) return undefined
-      return {
-        url: `https://goazen.info${eventUrl}`,
-        lastModified: new Date(),
-        changeFrequency: 'daily' as const,
-        priority: 0.5,
-      }
-    })
-    .filter((x): x is NonNullable<typeof x> => x !== undefined)
-
-  const specialEventsUrls = specialEvents.docs.map((event) => ({
-    url: `https://goazen.info/concerts/evenements/${event.slug}`,
-    lastModified: new Date(),
-    changeFrequency: 'monthly' as const,
-    priority: 1,
-  }))
-
+  const specialEventsUrls = specialEvents.docs.map((event) => {
+    return {
+      url: `https://goazen.info/concerts/evenements/${event.slug}`,
+      lastModified: new Date(),
+      changeFrequency: 'daily' as const,
+      priority: 1,
+    }
+  })
   const locations = await getLocations({ limit: 1000 })
-  const locationsUrls = locations.docs
-    .map((location) => {
-      const region = typeof location['city V2'] !== 'string' && location['city V2']?.region
-      const citySlug = typeof location['city V2'] !== 'string' && location['city V2']?.slug
-      if (!region || !citySlug) return undefined
-      return {
-        url: `https://goazen.info/concerts/${region}/${citySlug}/${location.slug}`,
-        lastModified: new Date(),
-        changeFrequency: 'monthly' as const,
-        priority: 0.8,
-      }
-    })
-    .filter((x): x is NonNullable<typeof x> => x !== undefined)
+  const locationsUrls = locations.docs.map((location) => {
+    const region = typeof location['city V2'] !== 'string' && location['city V2']?.region
+    const citySlug = typeof location['city V2'] !== 'string' && location['city V2']?.slug
+    return {
+      url: `https://goazen.info/concerts/${region}/${citySlug}/${location.slug}`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly' as const,
+      priority: 0.8,
+    }
+  })
 
   return [
     {
@@ -67,10 +63,10 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       changeFrequency: 'daily',
       priority: 1,
     },
-    ...specialEventsUrls,
+    ...eventsUrls,
+    ...locationsUrls,
     ...regionsUrls,
     ...citiesUrls,
-    ...locationsUrls,
-    ...eventsUrls,
+    ...specialEventsUrls,
   ]
 }
--- src/utils.ts
+++ src/utils.ts
@@ -58,14 +58,6 @@ export function createHref({
 
 export function buildEventUrl(event: Event) {
   const locationInfo = getLocationInfo(event)
-  if (
-    !locationInfo?.region ||
-    !event.region ||
-    !locationInfo.citySlug ||
-    !locationInfo.locationSlug
-  ) {
-    return null
-  }
   return `/concerts/${locationInfo?.region || event.region}/${locationInfo?.citySlug}/${locationInfo?.locationSlug}/${event.slug}_${event.id}`
 }
 
